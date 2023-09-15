f1fe963977a08e34e3e193bd209ab8f7b7b5cecf
Octavio Martinez <omartinez1618@gmail.com>
2023-09-14 23:43:31 -0400

    dd

--- src/routes/App/App.js
+++ src/routes/App/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with Zenx5 (papi)
+          Learn React with Zenx5
         </a>
       </header>
     </div>
